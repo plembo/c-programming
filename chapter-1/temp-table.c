c71b7a68d8600821a39bba7be41824ee04e0a63c
Phil Lembo <phil.lembo@gmail.com>
2021-05-23 13:04:30 -0400

    Use floating-point numbers instead of integers

--- chapter-1/temp-table.c
+++ chapter-1/temp-table.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-   for fahr = 0, 20, ... 30 */
+   for fahr = 0, 20, ... 30 *; floating-point version */
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;   /* lower limit of temperature table */
@@ -13,8 +13,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3d %6d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     } 
 }
